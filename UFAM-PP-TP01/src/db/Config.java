59b300f9a76590290f656d982297432c35994f4a
Elizeu Oliveira <homenzinhoptr@gmail.com>
2021-05-29 11:20:33 -0300

    upload sqlite lib

--- UFAM-PP-TP01/src/db/Config.java
+++ UFAM-PP-TP01/src/db/Config.java
@@ -3,8 +3,9 @@ package db;
 import java.sql.*;
 
 public class Config {
+    private static String javaDB = System.getenv("PWD") + "/UFAM-PP-TP01/src/db/moto.db";
     
-    private static String url = "jdbc:sqlite:/home/elizeu/Documents/UFAM/PP/TP01/src/db/moto.db";
+    private static String url = "jdbc:sqlite:/home/oliveira/Documentos/UFAM/PP/UFAM-PP-TP01/UFAM-PP-TP01/src/db/moto.db";
     protected static Connection conexao = null;
 
     public Config(){
@@ -12,6 +13,7 @@ public class Config {
     }
 
     private static boolean conecta(){
+        System.out.println(javaDB);
         try{
             conexao = DriverManager.getConnection(url);
             System.out.println("Conexao com sucesso!");
